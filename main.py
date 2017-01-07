a8ddff5eb6ce2a26003739df75a0885842d6a2b9
anebz <ane.berasategi@gmail.com>
2017-01-07 20:00:37 +0100

    encode function added

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
-
+# should be a .txt
 def openfile(filename):
-
   file = open(filename,'r') # 'r' for reading only
   str = file.read() #string with the whole text
   return str
 
+# given a tree in this format: {0:'a', 10:'b', 11:'c'}
+# and words being the string read from the file
+def encode(tree,words):
+	inv_tree = {v:k for k,v in tree.items()}
+  code = ''
+  for let in words:
+    if let in inv_tree:
+      code = code + str(inv_tree[let])
+  return code
+
+
 if __name__ == "__main__":
-  text = openfile('text_sample.txt')
-  print text
+  file = 'text_sample.txt'
+  text = openfile(file)
+  #print text
+  
+  # example cases
+  tree = {0:'a', 10:'b', 11:'c'}
+	words = 'acbca' 
+	code =  encode(tree,words)
+  # print code
