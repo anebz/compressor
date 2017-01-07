64f6819601182d18c416e183502401d9f54f994e
anebz <ane.berasategi@gmail.com>
2017-01-07 23:37:42 +0100

    writefile procedure added

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 # should be a .txt
-def openfile(filename):
+def readfile(filename):
   file = open(filename,'r') # 'r' for reading only
   str = file.read() #string with the whole text
   return str
+
+def writefile(filename, words):
+  f = open(filename, 'w')
+  f.write(words)
+  f.close()
 
 # given a tree in this format: {0:'a', 10:'b', 11:'c'}
 # and words being the string read from the file
@@ -16,12 +21,20 @@ def encode(tree,words):
 
 
 if __name__ == "__main__":
+  # open file
   file = 'text_sample.txt'
   text = openfile(file)
   #print text
   
-  # example cases
+  # example cases for encoding
   tree = {0:'a', 10:'b', 11:'c'}
   words = 'acbca' 
   code =  encode(tree,words)
   # print code
+
+  # write in file
+  extension = 'hff'
+  ex_filename = 'result' + '.' + extension
+  ex = open('text_sample.txt','r').read() #example to write
+  writefile(ex_filename, ex)
+  
