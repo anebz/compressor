2135b44a0e60d43ead23fd76036edc36fac8455c
Julen Cestero <julencestero.s8@gmail.com>
2017-01-08 21:18:50 +0100

    Working on ConstructHuffmanTree
    - Creating the ordered dictionary of used ASCII characters
    - Created a function to get the second smallest element in a numeric
    vector

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# Imports
+import operator
+
 # should be a .txt
 def readfile(filename):
   file = open(filename,'r') # 'r' for reading only
@@ -10,11 +13,13 @@ def writefile(filename, string):
   f.close()
 
 # returns a list with the frequencies of each letter in the string
+# returns a list with the frequencies of each letter in the string
 def frequency(string):
-  freq, leng = [], len(string)
+  freq, leng = {}, len(string)
   for i in range(127):
-    freq.append(string.count(chr(i))*1.0/leng)
-  return freq 
+    if string.count(chr(i)):
+      freq[chr(i)] = string.count(chr(i)*1.0/leng)
+  return freq
   
 # given a tree in this format: {0:'a', 10:'b', 11:'c'}
 # and words being the string, read from the file
@@ -26,6 +31,10 @@ def encode(tree,words):
       code = code + str(inv_tree[let])
   return code
 
+# returns a list with the Huffman-encoded ASCII table
+def ConstructHuffmanTree(text, count):
+  
+  
 
 if __name__ == "__main__":
   # open file
@@ -33,6 +42,11 @@ if __name__ == "__main__":
   text = openfile(file)
   file.close()
   #print text
+  
+  #Constructing the tree
+  characterCounter = frequency(text)
+  sortedCounter = sorted(characterCounter.items(), key=operator.itemgetter(1))
+  tree = constructHuffmanTree(text, sortedCounter)
   
   # example cases for encoding
   tree = {0:'a', 10:'b', 11:'c'}
